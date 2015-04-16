2cf1767cac9e2053b699b69c1bddd10a8e9deebe
Shuopeng LI <li.shuopeng@univ-paris13.fr>
2015-04-16 18:52:04 +0200

    alevin
    fisrt commit

---
+++ src/tset/Gra.java
@@ -0,0 +1,16 @@
+package tset;
+
+import java.io.IOException;
+
+import vnreal.network.substrate.SubstrateNetwork;
+
+public class Gra {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		SubstrateNetwork sn=new SubstrateNetwork(true);
+		sn.alt2network("data/vir0");
+		
+	}
+
+}
