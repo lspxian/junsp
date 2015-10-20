b555da08705b80ccf2277d17c3970bf49d61ada6
li.shuopeng <li.shuopeng@univ-paris13.fr>
2015-10-20 10:34:11 +0200

    augmented virtual link

---
+++ src/vnreal/network/virtual/AugmentedVirtualLink.java
@@ -0,0 +1,23 @@
+package vnreal.network.virtual;
+
+import li.multiDomain.Domain;
+
+public class AugmentedVirtualLink extends VirtualLink {
+	private VirtualLink originalVL;
+	private Domain originalDomain;
+	
+	public AugmentedVirtualLink(Domain d, VirtualLink ovl){
+		this.originalDomain = d;
+		this.originalVL = ovl;
+	}
+
+	public VirtualLink getOriginalVL() {
+		return originalVL;
+	}
+
+	public Domain getOriginalDomain() {
+		return originalDomain;
+	}
+	
+	
+}
