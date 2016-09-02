d4135d6a17c393c6fccae1edd1dc55273b8acafc
li.shuopeng <li.shuopeng@univ-paris13.fr>
2016-09-02 10:33:30 +0200

    02/09 merge

--- src/li/gt_itm/Generator.java
+++ src/li/gt_itm/Generator.java
@@ -37,8 +37,8 @@ public class Generator {
 		pw.println("geo 1 "+new Random().nextInt(100));
 		//node number, scale, method, proba connect
 		int number = new Random().nextInt(7)+3;
-//		int number = 2;
-		pw.println(number+" 100 3 0.3");
+//		int number = 3;
+		pw.println(number+" 100 3 0.5");
 		pw.close();
 		runShellCmd("./gt-itm/itm gt-itm/subCmd");
 		runShellCmd("./gt-itm/sgb2alt gt-itm/subCmd-0.gb gt-itm/vir");
